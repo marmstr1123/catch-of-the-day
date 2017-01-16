714b7711b1f2894590cccb1482d92daad819177d
marmstr1123 <michaelj.arm1@gmail.com>
2017-01-16 16:33:00 -0500

    CRUD functioning w/removeFish

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -31,6 +31,7 @@ class Inventory extends React.Component{
             <textarea type="text" name="desc" value={fish.desc} placeholder="Fish desc" onChange={(e) => this.handleChange(e,key)}>
             </textarea>
             <input type="text" name="image" value={fish.image} placeholder="Fish image" onChange={(e) => this.handleChange(e,key)}/>
+            <button onClick={(e)=> this.props.removeFish(key)}>Remove Fish</button>
          </div>
       )
    }
